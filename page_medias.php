e70e3455416de1a86c7e64fcdc3fc9569b853e79
Isses <alexandre.croizard@lanfoster.fr>
2015-12-03 11:29:51 +0100

    Dynamisation de la page Medias

--- page_medias.php
+++ page_medias.php
@@ -21,76 +21,50 @@
 	<section class="transition">
 		<div class="content"></div>
 	</section>
+	
+	<?  $query = new WP_Query(array('post_type'=>'album', 'order'=>'DESC', 'posts_per_page' => -1 ));
+		$mediaList = $query->posts;
+		$photosList = array();
+		$videosList = array();
+		foreach ($mediaList as $key => $album) { 
+			$infos = get_post_meta( $album->ID );
+			if( $infos['type'][0] == 'Photos' ){
+				array_push( $photosList, $album );
+			} else {
+				array_push( $videosList, $album );
+			}
+		}
 
+	?>
 	<section class="enphotos">
 		<h2>EN PHOTOS</h2>
 
 		<div class="wall">
+		<?
+		foreach ($photosList as $album) { 
+			$infos = get_post_meta( $album->ID );
+            $mediasCount = maybe_unserialize($infos['mediaCount'][0]);
+            $urlCouverture = wp_get_attachment_url( get_post_thumbnail_id($album->ID) );
+		?>
 			<div class="littleline white hiddenBlock rollimage_parent_vertical">
 				<div class="rollimage">
-					<div class="img" style="background:url('static/img/ecole/desmond.jpg') no-repeat center;"></div>
+					<div class="img" style="background-image: url('<?= $urlCouverture; ?>');" ></div>
 				</div>
 				<div class="text">
-					<div class="title">concert de fin d’année 2015</div>
-					<div class="baseline">10 Photos</div>
+					<div class="title"><?= $album->post_title ?></div>
+					<div class="baseline">
+				 		<?
+                            echo $mediasCount .' ';
+                            if( $mediasCount > 1 ) echo 'photos';
+                            else echo 'photo'; 
+                        ?>
+					</div>
 					<div class="moreinfosbtn">
 						<div class="button">VOIR</div>
 					</div>
 				</div>
 			</div>
-			<div class="littleline orange hiddenBlock rollimage_parent_vertical">
-				<div class="rollimage">
-					<div class="img" style="background:url('static/img/ecole/desmond.jpg') no-repeat center;"></div>
-				</div>
-				<div class="text">
-					<div class="title">concert de fin d’année 2015</div>
-					<div class="baseline">10 Photos</div>
-					<div class="moreinfosbtn">
-						<div class="button">VOIR</div>
-					</div>
-				</div>
-			</div>
-			<div class="littleline lightpurple hiddenBlock rollimage_parent_vertical">
-				<div class="rollimage">
-					<div class="img" style="background:url('static/img/ecole/desmond.jpg') no-repeat center;"></div>
-				</div>
-				<div class="text">
-					<div class="title">concert de fin d’année 2015</div>
-					<div class="baseline">10 Photos</div>
-					<div class="moreinfosbtn">
-						<div class="button">VOIR</div>
-					</div>
-				</div>
-			</div>
-			<div class="littleline darkpurple hiddenBlock rollimage_parent_vertical">
-				<div class="rollimage">
-					<div class="img" style="background:url('static/img/ecole/desmond.jpg') no-repeat center;"></div>
-				</div>
-				<div class="text">
-					<div class="title">concert de fin d’année 2015</div>
-					<div class="baseline">10 Photos</div>
-					<div class="moreinfosbtn">
-						<div class="button">VOIR</div>
-					</div>
-				</div>
-			</div>
-			<div class="littleline fullimg hiddenBlock rollimage_parent_vertical">
-				<div class="rollimage">
-					<div class="img" style="background:url('static/img/ecole/desmond.jpg') no-repeat center;"></div>
-				</div>
-			</div>
-			<div class="littleline darkpurple hiddenBlock rollimage_parent_vertical">
-				<div class="rollimage">
-					<div class="img" style="background:url('static/img/ecole/desmond.jpg') no-repeat center;"></div>
-				</div>
-				<div class="text">
-					<div class="title">concert de fin d’année 2015</div>
-					<div class="baseline">10 Photos</div>
-					<div class="moreinfosbtn">
-						<div class="button">VOIR</div>
-					</div>
-				</div>
-			</div>
+		<? } ?>
 		</div>
 		<div>
 			<div class="button morepictures">+ DE PHOTOS</div>
@@ -105,71 +79,34 @@
 		<h2>EN VIDÉOS</h2>
 
 		<div class="wall">
+		<?
+		foreach ($videosList as $album) { 
+			$infos = get_post_meta( $album->ID );
+            $mediasCount = maybe_unserialize($infos['mediaCount'][0]);
+            $urlCouverture = wp_get_attachment_url( get_post_thumbnail_id($album->ID) );
+		?>
 			<div class="littleline white hiddenBlock rollimage_parent_vertical">
 				<div class="rollimage">
-					<div class="img" style="background:url('static/img/ecole/desmond.jpg') no-repeat center;"></div>
+					<div class="img" style="background-image: url('<?= $urlCouverture; ?>');" ></div>
 				</div>
 				<div class="text">
-					<div class="title">concert de fin d’année 2015</div>
-					<div class="baseline">10 Photos</div>
+					<div class="title"><?= $album->post_title ?></div>
+					<div class="baseline">
+				 		<?
+                            echo $mediasCount .' ';
+                            if( $mediasCount > 1 ) echo 'vidéos';
+                            else echo 'vidéo';
+                        ?>
+					</div>
 					<div class="moreinfosbtn">
 						<div class="button">VOIR</div>
 					</div>
 				</div>
 			</div>
-			<div class="littleline orange hiddenBlock rollimage_parent_vertical">
-				<div class="rollimage">
-					<div class="img" style="background:url('static/img/ecole/desmond.jpg') no-repeat center;"></div>
-				</div>
-				<div class="text">
-					<div class="title">concert de fin d’année 2015</div>
-					<div class="baseline">10 Photos</div>
-					<div class="moreinfosbtn">
-						<div class="button">VOIR</div>
-					</div>
-				</div>
-			</div>
-			<div class="littleline lightpurple hiddenBlock rollimage_parent_vertical">
-				<div class="rollimage">
-					<div class="img" style="background:url('static/img/ecole/desmond.jpg') no-repeat center;"></div>
-				</div>
-				<div class="text">
-					<div class="title">concert de fin d’année 2015</div>
-					<div class="baseline">10 Photos</div>
-					<div class="moreinfosbtn">
-						<div class="button">VOIR</div>
-					</div>
-				</div>
-			</div>
-			<div class="littleline darkpurple hiddenBlock rollimage_parent_vertical">
-				<div class="rollimage">
-					<div class="img" style="background:url('static/img/ecole/desmond.jpg') no-repeat center;"></div>
-				</div>
-				<div class="text">
-					<div class="title">concert de fin d’année 2015</div>
-					<div class="baseline">10 Photos</div>
-					<div class="moreinfosbtn">
-						<div class="button">VOIR</div>
-					</div>
-				</div>
-			</div>
-			<div class="littleline fullimg hiddenBlock rollimage_parent_vertical">
-				<div class="rollimage">
-					<div class="img" style="background:url('static/img/ecole/desmond.jpg') no-repeat center;"></div>
-				</div>
-			</div>
-			<div class="littleline darkpurple hiddenBlock rollimage_parent_vertical">
-				<div class="rollimage">
-					<div class="img" style="background:url('static/img/ecole/desmond.jpg') no-repeat center;"></div>
-				</div>
-				<div class="text">
-					<div class="title">concert de fin d’année 2015</div>
-					<div class="baseline">10 Photos</div>
-					<div class="moreinfosbtn">
-						<div class="button">VOIR</div>
-					</div>
-				</div>
-			</div>
+		<? } ?>
+		</div>
+		<div>
+			<div class="button morepictures">+ DE VIDÉOS</div>
 		</div>
 	</section>
 
